1e51c82468a0f703738ddebc8a9599ac57417895
spoltier <simon.poltier@mirai-solutions.com>
2019-10-02 09:17:42 +0200

    XLConnect #37 - small code cleanup

--- src/main/java/com/miraisolutions/xlconnect/integration/r/RWorkbookWrapper.java
+++ src/main/java/com/miraisolutions/xlconnect/integration/r/RWorkbookWrapper.java
@@ -162,7 +162,7 @@ public final class RWorkbookWrapper {
     }
 
     public RDataFrameWrapper readWorksheet(String worksheet, int startRow, int startCol, int endRow, int endCol, 
-            boolean header, String colTypes[], boolean forceConversion, String dateTimeFormat, boolean takeCached, 
+            boolean header, String colTypes[], boolean forceConversion, String dateTimeFormat, boolean takeCached,
             int [] subset, String readStrategy) {
         DataFrame dataFrame = workbook.readWorksheet(worksheet, startRow, startCol, endRow, endCol, header,
                 ReadStrategy.valueOf(readStrategy.toUpperCase()), dataTypeFromString(colTypes), forceConversion, dateTimeFormat,
@@ -235,7 +235,7 @@ public final class RWorkbookWrapper {
     }
 
     public void addImage(String filename, String name, boolean originalSize)
-            throws FileNotFoundException, IOException {
+            throws IOException {
         workbook.addImage(filename, name, originalSize);
     }
 
